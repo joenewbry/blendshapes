68f9820db1de39eee794c65b4b21138eb8724750
Joe Newbry <joenewbry@gmail.com>
2024-06-10 10:39:42 -0400

    First approach to saving blend shape data.
    Next steps
    1. Add live preview so that you can see recording as it happens
    2. Fix up csv capture so that it works for multiple time segments

--- ARKitFaceExample/ViewController.swift
+++ ARKitFaceExample/ViewController.swift
@@ -1,78 +1,58 @@
-/*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-Main view controller for the AR experience.
-*/
-
 import ARKit
 import SceneKit
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController, ARSessionDelegate {
-    
+class ViewController: UIViewController, ARSessionDelegate, ARSCNViewDelegate {
+
     // MARK: Outlets
-
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet weak var tabBar: UITabBar!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
 
     // MARK: Properties
-    
     var faceAnchorsAndContentControllers: [ARFaceAnchor: VirtualContentController] = [:]
-    
     var selectedVirtualContent: VirtualContentType! {
         didSet {
-            guard oldValue != nil, oldValue != selectedVirtualContent
-                else { return }
-            
-            // Remove existing content when switching types.
+            guard oldValue != nil, oldValue != selectedVirtualContent else { return }
             for contentController in faceAnchorsAndContentControllers.values {
                 contentController.contentNode?.removeFromParentNode()
             }
-            
-            // If there are anchors already (switching content), create new controllers and generate updated content.
-            // Otherwise, the content controller will place it in `renderer(_:didAdd:for:)`.
             for anchor in faceAnchorsAndContentControllers.keys {
                 let contentController = selectedVirtualContent.makeController()
                 if let node = sceneView.node(for: anchor),
-                let contentNode = contentController.renderer(sceneView, nodeFor: anchor) {
+                   let contentNode = contentController.renderer(sceneView, nodeFor: anchor) {
                     node.addChildNode(contentNode)
                     faceAnchorsAndContentControllers[anchor] = contentController
                 }
             }
         }
     }
-    
-    // MARK: - View Controller Life Cycle
+
+    var isRecording = false
+    var videoOutput: AVCaptureMovieFileOutput?
+    var session: AVCaptureSession?
+    var blendShapeData: [ARFaceAnchor.BlendShapeLocation: NSNumber] = [:]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         sceneView.delegate = self
         sceneView.session.delegate = self
         sceneView.automaticallyUpdatesLighting = true
-        
-        // Set the initial face content.
         tabBar.selectedItem = tabBar.items!.first!
         selectedVirtualContent = VirtualContentType(rawValue: tabBar.selectedItem!.tag)
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        // AR experiences typically involve moving the device without
-        // touch input for some time, so prevent auto screen dimming.
         UIApplication.shared.isIdleTimerDisabled = true
-        
-        // "Reset" to run the AR session for the first time.
         resetTracking()
     }
 
     // MARK: - ARSessionDelegate
-
     func session(_ session: ARSession, didFailWithError error: Error) {
         guard error is ARError else { return }
-        
         let errorWithInfo = error as NSError
         let messages = [
             errorWithInfo.localizedDescription,
@@ -80,29 +60,21 @@ class ViewController: UIViewController, ARSessionDelegate {
             errorWithInfo.localizedRecoverySuggestion
         ]
         let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
-        
         DispatchQueue.main.async {
             self.displayErrorMessage(title: "The AR session failed.", message: errorMessage)
         }
     }
-    
-    /// - Tag: ARFaceTrackingSetup
+
     func resetTracking() {
         guard ARFaceTrackingConfiguration.isSupported else { return }
         let configuration = ARFaceTrackingConfiguration()
-        if #available(iOS 13.0, *) {
-            configuration.maximumNumberOfTrackedFaces = ARFaceTrackingConfiguration.supportedNumberOfTrackedFaces
-        }
         configuration.isLightEstimationEnabled = true
         sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
-        
         faceAnchorsAndContentControllers.removeAll()
     }
-    
+
     // MARK: - Error handling
-    
     func displayErrorMessage(title: String, message: String) {
-        // Present an alert informing about the error that has occurred.
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
             alertController.dismiss(animated: true, completion: nil)
@@ -111,57 +83,100 @@ class ViewController: UIViewController, ARSessionDelegate {
         alertController.addAction(restartAction)
         present(alertController, animated: true, completion: nil)
     }
-    
-    // Auto-hide the home indicator to maximize immersion in AR experiences.
+
     override var prefersHomeIndicatorAutoHidden: Bool {
         return true
     }
-    
-    // Hide the status bar to maximize immersion in AR experiences.
+
     override var prefersStatusBarHidden: Bool {
         return true
     }
-}
 
-extension ViewController: UITabBarDelegate {
-    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        guard let contentType = VirtualContentType(rawValue: item.tag)
-            else { fatalError("unexpected virtual content tag") }
-        selectedVirtualContent = contentType
-    }
-}
-
-extension ViewController: ARSCNViewDelegate {
-        
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        guard let faceAnchor = anchor as? ARFaceAnchor else { return }
-        
-        // If this is the first time with this anchor, get the controller to create content.
-        // Otherwise (switching content), will change content when setting `selectedVirtualContent`.
-        DispatchQueue.main.async {
-            let contentController = self.selectedVirtualContent.makeController()
-            if node.childNodes.isEmpty, let contentNode = contentController.renderer(renderer, nodeFor: faceAnchor) {
-                node.addChildNode(contentNode)
-                self.faceAnchorsAndContentControllers[faceAnchor] = contentController
-            }
+    // MARK: - Recording Methods
+    @IBAction func recordButtonTapped(_ sender: UIButton) {
+        if isRecording {
+            stopRecording()
+        } else {
+            startRecording()
         }
     }
-    
-    /// - Tag: ARFaceGeometryUpdate
+
+    func startRecording() {
+        isRecording = true
+        recordButton.setTitle("Stop Recording", for: .normal)
+        // Add video recording setup here
+        startVideoRecording()
+    }
+
+    func stopRecording() {
+        isRecording = false
+        recordButton.setTitle("Start Recording", for: .normal)
+        // Add video recording stop logic here
+        stopVideoRecording()
+    }
+
+    func startVideoRecording() {
+        session = AVCaptureSession()
+        guard let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        session?.addInput(input)
+
+        videoOutput = AVCaptureMovieFileOutput()
+        session?.addOutput(videoOutput!)
+        session?.startRunning()
+
+        let outputPath = NSTemporaryDirectory() + "output.mov"
+        let outputURL = URL(fileURLWithPath: outputPath)
+        videoOutput?.startRecording(to: outputURL, recordingDelegate: self)
+    }
+
+    func stopVideoRecording() {
+        videoOutput?.stopRecording()
+        session?.stopRunning()
+        session = nil
+    }
+
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        let outputPath = NSTemporaryDirectory() + "output.mov"
+        let outputURL = URL(fileURLWithPath: outputPath)
+        let csvPath = NSTemporaryDirectory() + "blendshapes.csv"
+        let csvURL = URL(fileURLWithPath: csvPath)
+        let activityViewController = UIActivityViewController(activityItems: [outputURL, csvURL], applicationActivities: nil)
+        present(activityViewController, animated: true, completion: nil)
+    }
+
+    // MARK: - BlendShape Data Handling
+    func saveBlendShapeData() {
+        let csvPath = NSTemporaryDirectory() + "blendshapes.csv"
+        let csvURL = URL(fileURLWithPath: csvPath)
+        var csvText = "BlendShapeLocation,Value\n"
+        for (location, value) in blendShapeData {
+            csvText += "\(location),\(value)\n"
+        }
+        do {
+            try csvText.write(to: csvURL, atomically: true, encoding: .utf8)
+        } catch {
+            print("Failed to create file")
+            print("\(error)")
+        }
+    }
+
+    // MARK: - ARSCNViewDelegate
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        guard let faceAnchor = anchor as? ARFaceAnchor,
-            let contentController = faceAnchorsAndContentControllers[faceAnchor],
-            let contentNode = contentController.contentNode else {
-            return
-        }
-        
-        contentController.renderer(renderer, didUpdate: contentNode, for: anchor)
-    }
-    
-    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
         guard let faceAnchor = anchor as? ARFaceAnchor else { return }
-        
-        faceAnchorsAndContentControllers[faceAnchor] = nil
+        blendShapeData = faceAnchor.blendShapes
+        if isRecording {
+            saveBlendShapeData()
+        }
     }
 }
 
+extension ViewController: AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        if let error = error {
+            print("Recording error: \(error.localizedDescription)")
+        } else {
+            print("Recording finished: \(outputFileURL)")
+        }
+    }
+}
